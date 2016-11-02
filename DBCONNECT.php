b9645c68b0b85fd34f77d858307b2e925aea7f4c
azifchyy <azifchyy>
2016-11-02 16:42:21 +0200

    Adminer commit
    dbconnect.php commiting

--- DBCONNECT.php
+++ DBCONNECT.php
@@ -14,3 +14,16 @@ define('DB_DATABASE'. 'database');
 $db = mysql_connect(DB_SERVER,
     DB_USERNAME, DB2_PASSWORD,
     DB_DATABASE);
+
+$db = new mysqli(
+    'eu-cdbr-azure-west-a.cloudapp.net',
+    'b6e9aa1b391bb0',
+    '42f2d8ac',
+    'ijos_database'
+);
+
+if($db->connect_errno){
+    die('Connectfailed['.$db->connect_error.']');
+}
+
+?>
