d17a5fa01159782ddebe9360216e9ee20ed7e97d
azifchyy <azifchyy>
2016-11-02 17:36:33 +0200

    Adminer commit
    dbconnect.php commiting

---
+++ week3/connecting.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: azifchyy
+ * Date: 2.11.2016 г.
+ * Time: 17:36
+ */
+$db = new mysqli(
+    'eu-cdbr-azure-west-a.cloudapp.net',
+    'b6e9aa1b391bb0',
+    '42f2d8ac',
+    'ijos_database'
+);
+//test to see if connection is established
+if($db->connect_errno){
+    die('Connectfailed['.$db->connect_error.']');
+}
+?>
