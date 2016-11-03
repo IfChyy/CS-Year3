40eae85181bc546f4b9c48dd6428102fba6b0c6a
azifchyy <azifchyy>
2016-11-03 18:28:26 +0200

    Adminer commit
    dbconnect.php commiting

--- week3/displayAllMovies.php
+++ week3/displayAllMovies.php
@@ -12,7 +12,7 @@ $sql_query = "Select * from marvelmovies";
 $result = $db->query($sql_query);
 
 while($row = $result->fetch_array()){
-    echo "<p>". $row[marvelMoviesID] . "</p>";
+    echo "<p> -- $row[title] -- </p>";
 }
 
 ?>
