b83ab76b54b07f3dddffcf9e5e07d2cf6e499157
azifchyy <azifchyy>
2016-11-03 18:49:20 +0200

    Adminer commit
    dbconnect.php commiting

---
+++ week3/moviesByMarvel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: azifchyy
+ * Date: 3.11.2016 г.
+ * Time: 18:46
+ */
+
+include("DBCONNECT.php");
+
+$sql_query = "SELECT * FROM marvelmovies WHERE productionStudios LIKE Marvel Studios";
+
+$result = $db->query($sql_query);
+
+while($row = $result->fetch_array()){
+    echo "<p>  $row[yearReleased] , $row[title], $row[productionStudio]  </p>";
+}
