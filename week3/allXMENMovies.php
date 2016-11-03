d4b37f420bd777e63165a19051b8378406cea1bb
azifchyy <azifchyy>
2016-11-03 21:02:02 +0200

    Adminer commit
    dbconnect.php commiting

---
+++ week3/allXMENMovies.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: azifchyy
+ * Date: 3.11.2016 г.
+ * Time: 20:59
+ */
+
+include("DBCONNECT.php");
+
+$sql_query = "SELECT * FROM marvelmovies WHERE title LIKE %X-MEN%";
+
+$result = $db->query($sql_query);
+
+while($row = $result->fetch_array()){
+    echo "<p> $row[title], $row[productionStudio]</p>";
+}
