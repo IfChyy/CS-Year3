5f6e38d3b075cbd7bc663e4ff51dc7ea45d4a12d
azifchyy <azifchyy>
2016-11-05 17:48:11 +0200

    Adminer commit
    dbconnect.php commiting

--- week4/helloSelector.php
+++ week4/helloSelector.php
@@ -20,8 +20,9 @@
     <form action="formResponce.php" method="post">
         <label>Forename</label><input type="text" name="forename">
         <label>Surname</label><input type="text" name="surname">
-        <input type="sumbit" value="Submit">
+        <input type="submit" value="Submit">
     </form>
+
 
 </ul>
 
