66a15d6682aebd832cf20249569444ef1789920b
azifchyy <azifchyy>
2016-11-05 17:47:11 +0200

    Adminer commit
    dbconnect.php commiting

--- week4/helloSelector.php
+++ week4/helloSelector.php
@@ -16,11 +16,13 @@
     <li><a href="http://cs-year3.azurewebsites.net/week4/helloPrinter.php?username=Mars">Mars</a></li>
 
     <br>
+
     <form action="formResponce.php" method="post">
-        <label>Forename</label><input type=“text” name=“forename”>
-        <label>Surname</label><input type=“text” name=“surname”>
-        <input type="submit" value="Submit">
+        <label>Forename</label><input type="text" name="forename">
+        <label>Surname</label><input type="text" name="surname">
+        <input type="sumbit" value="Submit">
     </form>
+
 </ul>
 
 
