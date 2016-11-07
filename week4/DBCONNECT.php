80c931be775ca04015986f0cc986514190f2d7ce
azifchyy <azifchyy>
2016-11-07 17:30:42 +0200

    Adminer commit
    dbconnect.php commiting

--- week4/DBCONNECT.php
+++ week4/DBCONNECT.php
@@ -1,21 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: azifchyy
- * Date: 5.11.2016 г.
- * Time: 18:25
- */
-
-
-define('DB_SERVER'. 'eu-cdbr-azure-west-a.cloudapp.net');
-define('DB_USERNAME'. 'b6e9aa1b391bb0');
-define('DB_PASSWORD'. '42f2d8ac');
-define('DB_DATABASE'. 'ijos_database');
-
-
-$db = mysqli_connect(DB_SERVER,
-    DB_USERNAME, DB_PASSWORD,
-    DB_DATABASE);
 
 $db = new mysqli(
     'eu-cdbr-azure-west-a.cloudapp.net',
